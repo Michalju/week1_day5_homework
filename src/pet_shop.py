c8d2d6a20256ea8d14a1d3bb58218b8d89276d6c
Michalju <juszkin@gmail.com>
2020-11-06 15:15:41 +0000

    Added get_total_cash function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,3 +4,8 @@
 #   - To return name of a given pet shop
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
+
+# The purpose of the function:
+#   - To return the total cash of a given pet shop
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
