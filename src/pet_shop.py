b6574a7beddd87671ed4f4c242c7a427ec47d3f0
Michalju <juszkin@gmail.com>
2020-11-06 15:28:08 +0000

    Added add_or_remove_cash function which either add or remove cash for a given pet shop

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -9,3 +9,9 @@ def get_pet_shop_name(pet_shop):
 #   - To return the total cash of a given pet shop
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+# The purpose of the function:
+#   - To add cash for a given pet shop
+#   - To remove cash for a given pet shop
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] +=  cash
